eb92e88f80f9cb95b9f36af221f889f8001e7c04
Isabela Popa <isabela.popa87@gmail.com>
2018-11-14 15:05:42 +0200

    Update search text state

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 
 class SearchBooks extends React.Component {
 
+    state = {
+        searchText: ''
+    }
+
+    updateSearchText = (searchText) => {
+        this.setState({ searchText: searchText })
+    }
+
     render() {
         return (
 
@@ -15,15 +23,13 @@ class SearchBooks extends React.Component {
                         to="/"
                     >Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author" />
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.searchText}
+                            onChange={(event) => this.updateSearchText(event.target.value)}
+                        />
 
                     </div>
                 </div>

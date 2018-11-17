06881607ea533a2e21fe7f8b8e29d9ddce40aa5f
Isabela Popa <isabela.popa87@gmail.com>
2018-11-17 14:30:49 +0200

    Handle missing book thumbnail

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ class Book extends React.Component {
 
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''})` }}></div>
                     <ChangeBookShelf />
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
